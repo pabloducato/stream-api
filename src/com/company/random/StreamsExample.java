dbff1e9dbd5aafa16743087297d6ebb0b55e9d46
Paweł Kocan <pkocan@onet.pl>
2021-08-09 21:53:41 +0200

    Exercises - next part

--- src/com/company/random/StreamsExample.java
+++ src/com/company/random/StreamsExample.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsExample {
 
@@ -131,5 +133,156 @@ public class StreamsExample {
                 .count();
         System.out.println(numberOfEmployees);
     }
+
+    @Test
+    public void skipOperation2() {
+        employees.stream().sorted(Comparator.comparing(Employee::getLastName))
+                .skip(2)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void countOperation2() {
+        long numberOfEmployees = employees.stream()
+                .filter(employee -> employee.getFirstName().startsWith("B"))
+                .count();
+
+        System.out.println(numberOfEmployees);
+    }
+
+    @Test
+    public void minMaxOperations() {
+        final Employee youngestEmployee = employees.stream()
+                .min(Comparator.comparing(Employee::getAge)).get();
+        System.out.println(youngestEmployee);
+    }
+
+    @Test
+    public void maxMinOperations() {
+        final Optional<Employee> oldestEmployee = Optional.of(employees.stream()
+                .max(Comparator.comparing(Employee::getAge)).get());
+        System.out.println(oldestEmployee);
+    }
+
+    @Test
+    public void findAnyFindFirstOperations() {
+        final Employee employee = employees.stream()
+                .filter(emp -> emp.getFirstName().startsWith("B"))
+                .findFirst().get();
+        System.out.println(employee);
+    }
+
+    @Test
+    public void findAnyFindFirstOperationsAny() {
+        final Employee employee = employees.stream()
+                .filter(emp -> emp.getFirstName().startsWith("B"))
+                .findAny().get();
+        System.out.println(employee);
+    }
+
+    @Test
+    public void matchOperations() {
+        final boolean b = employees.stream()
+                .allMatch(emp -> emp.getFirstName().startsWith("B"));
+
+        System.out.println(b);
+    }
+
+    @Test
+    public void matchOperations2() {
+        final boolean b = employees.stream()
+                .allMatch(emp -> emp.getFirstName().contains("a"));
+
+        System.out.println(b);
+    }
+
+    @Test
+    public void matchOperations3() {
+        final boolean b = employees.stream()
+                .anyMatch(emp -> emp.getFirstName().contains("a"));
+
+        System.out.println(b);
+    }
+
+    @Test
+    public void matchOperations4() {
+        final boolean b = employees.stream()
+                .noneMatch(emp -> emp.getFirstName().startsWith("Z"));
+
+        System.out.println(b);
+    }
+
+    @Test
+    public void reduce() {
+        final Integer sumOfAllAges = employees.stream()
+                .map(Employee::getAge)
+                .reduce(Integer::sum)
+                .get();
+        System.out.println(sumOfAllAges);
+    }
+
+    @Test
+    public void reduce2() {
+        final Integer sumOfAllAges2 = employees.stream()
+                .map(Employee::getAge)
+                .reduce(0, Integer::sum);
+        System.out.println(sumOfAllAges2);
+    }
+
+    @Test
+    public void reduce3() {
+        final Integer sumOfAllAges3 = employees.stream()
+                .reduce(0, (age, employee) -> age + employee.getAge(), Integer::sum);
+        System.out.println(sumOfAllAges3);
+    }
+
+    @Test
+    public void addCommas() {
+        final String allNames = employees.stream()
+                .map(Employee::getFirstName)
+                .reduce((name, name2) -> name + ", " + name2)
+                .get();
+
+        System.out.println(allNames);
+    }
+
+    @Test
+    public void takeWhileOperation() {
+        final Stream<Employee> employeeStream = employees.stream()
+                .sorted(Comparator.comparing(Employee::getAge))
+                .takeWhile(employee -> employee.getAge() < 30);
+        employeeStream
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void dropWhileOperation() {
+        employees.stream()
+                .sorted(Comparator.comparing(Employee::getAge))
+                .dropWhile(employee -> employee.getAge() < 30)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void forEachOrdered() {
+        String sentence = "Jak nauczyc sie programowania";
+        sentence.chars().forEach(s -> System.out.print((char) s));
+        System.out.println();
+        sentence.chars().parallel().forEach(s -> System.out.print((char) s));
+        System.out.println();
+        sentence.chars().parallel().forEachOrdered(s -> System.out.print((char) s));
+    }
+
+    //anti-pattern -> not use it anymore
+    @Test
+    public void peekOperation() {
+        final List<Employee> newEmployees = employees.stream()
+                .peek(employee -> employee.setFirstName("Kamil"))
+                .collect(Collectors.toList());
+        System.out.println(newEmployees);
+        System.out.println();
+        System.out.println(employees);
+    }
+
 
 }
